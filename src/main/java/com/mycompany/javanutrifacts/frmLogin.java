a56725c87d0632c31a1b0ea893b99c232072b0c2
Carlos <Carlos@CARLOS-PC>
2024-10-01 09:34:45 -0500

    Avance de Login

--- src/main/java/com/mycompany/javanutrifacts/frmLogin.java
+++ src/main/java/com/mycompany/javanutrifacts/frmLogin.java
@@ -28,55 +28,71 @@ public class frmLogin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jlblusuario = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jlblbienvenido = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jlblpassword = new javax.swing.JLabel();
+        jlblusuario = new javax.swing.JLabel();
+        jcmbnombre = new javax.swing.JComboBox<>();
+        jlblfoto = new javax.swing.JLabel();
+        jbtniniciar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("VENTANA DE INICIO DE SESION");
+        setBackground(new java.awt.Color(0, 0, 0));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setName("frmlogin"); // NOI18N
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Ingresar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 220, -1, -1));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder(""), "LOGIN DE USUARIOS", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 3, 18), new java.awt.Color(255, 139, 1))); // NOI18N
+        jPanel1.setForeground(new java.awt.Color(242, 116, 2));
+        jPanel1.setMinimumSize(new java.awt.Dimension(600, 600));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 80, -1, -1));
-
-        jlblusuario.setText("Usuario");
-        getContentPane().add(jlblusuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, -1, -1));
-
-        jLabel2.setText("jLabel2");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
+        jlblbienvenido.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jlblbienvenido.setText("Bienvenido");
+        jPanel1.add(jlblbienvenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 50, 140, -1));
 
         jPasswordField1.setText("jPasswordField1");
-        getContentPane().add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 130, -1, -1));
+        jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 160, -1, -1));
 
-        jLabel3.setText("jLabel3");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 70, 90, 120));
+        jlblpassword.setText("Password");
+        jPanel1.add(jlblpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, -1, -1));
 
-        jlblbienvenido.setText("Bienvenido");
-        getContentPane().add(jlblbienvenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(117, 30, 140, -1));
+        jlblusuario.setText("Usuario");
+        jPanel1.add(jlblusuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 110, -1, -1));
+
+        jcmbnombre.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jPanel1.add(jcmbnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 110, -1, -1));
+
+        jlblfoto.setText("jLabel3");
+        jPanel1.add(jlblfoto, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 90, 120, 160));
+
+        jbtniniciar.setText("Iniciar Sesion");
+        jbtniniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtniniciarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jbtniniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 280, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 540, 380));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jbtniniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtniniciarActionPerformed
         frmMenu fm = new frmMenu();
         fm.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
+        String Usuario = jcmbnombre.getSelectedItem().toString()+".jpg";
+        String Password = jPasswordField1.getText();
+        
+        if(Usuario.isEmpty())(Password.isEmtpy()){
+        
+    }
+    }//GEN-LAST:event_jbtniniciarActionPerformed
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -110,12 +126,13 @@ public class frmLogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JButton jbtniniciar;
+    private javax.swing.JComboBox<String> jcmbnombre;
     private javax.swing.JLabel jlblbienvenido;
+    private javax.swing.JLabel jlblfoto;
+    private javax.swing.JLabel jlblpassword;
     private javax.swing.JLabel jlblusuario;
     // End of variables declaration//GEN-END:variables
 }
